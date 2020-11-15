8463b5a3bd3f708d05b3f4cff36cb7172fb12ae7
jerry <jerry@zy.com>
2020-11-15 17:09:38 +0800

    spring-boot-rocketmq

--- dubbo-spring-cloud-consumer/src/main/java/com/zy/consumer/rocketmq/RocketmqConsumer.java
+++ dubbo-spring-cloud-consumer/src/main/java/com/zy/consumer/rocketmq/RocketmqConsumer.java
@@ -1,5 +1,9 @@
 package com.zy.consumer.rocketmq;
 
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Component;
+
 /**
  * 消息消费者骤分析
  * 1.创建消费者 Consumer, 指定消费者组名
@@ -8,5 +12,13 @@ package com.zy.consumer.rocketmq;
  * 4.设置回调函数，处理肖息
  * 5.启动消费者 Consumer
  */
-public class RocketmqConsumer {
+@RocketMQMessageListener(topic = "${rocketmq.custom.topic}",
+        selectorExpression ="${rocketmq.custom.tags}",
+        consumerGroup = "${rocketmq.custom.consumer-group}")
+@Component
+public class RocketmqConsumer implements RocketMQListener<Object> {
+    @Override
+    public void onMessage(Object message) {
+        System.out.println("消费者收到消息: " + message);
+    }
 }
--- dubbo-spring-cloud-producer/src/main/java/com/zy/producer/rocketmq/RocketmqProducer.java
+++ dubbo-spring-cloud-producer/src/main/java/com/zy/producer/rocketmq/RocketmqProducer.java
@@ -1,9 +1,17 @@
 package com.zy.producer.rocketmq;
 
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
 /**
  * 可以参考书籍 <<RocketMQ实战与原理解析>>
  * 这里的 demo 暂时放到了 本工程的 test 包下
- *
+ * <p>
  * 消息发送者步骤分析
  * 1.创建消息生产者 Producer,并指定生产者组名
  * 2.指定 nameserver 地址
@@ -12,5 +20,21 @@ package com.zy.producer.rocketmq;
  * 5.发送消息
  * 6.关闭生产者 Producer
  */
+@Component
 public class RocketmqProducer {
+
+    @Value("${rocketmq.custom.topic}")
+    private String topic;
+
+    @Value("${rocketmq.custom.tags}")
+    private String tags;
+
+    @Resource
+    private RocketMQTemplate mqTemplate;
+
+    public void send(final Object object) {
+        Message<Object> message = MessageBuilder.withPayload(object).build();
+        mqTemplate.send(String.format("%s:%s", topic, tags), message);
+    }
+
 }
