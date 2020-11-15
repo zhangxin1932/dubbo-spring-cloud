e5be0bf4c6fe1e42330433c347f3da92eadd5bb8
jerry <jerry@zy.com>
2020-11-15 22:06:03 +0800

    spring-boot- yml & 配置读取

--- spring-boot-mail-freemarker/src/main/java/com/zy/SpringBootMailFreemarkerApp.java
+++ spring-boot-mail-freemarker/src/main/java/com/zy/SpringBootMailFreemarkerApp.java
@@ -1,12 +1,28 @@
 package com.zy;
 
+import com.zy.config.ymlbean.YmlBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 //@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 600)
 @SpringBootApplication
 public class SpringBootMailFreemarkerApp {
+
+    @Autowired
+    private YmlBean ymlBean;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMailFreemarkerApp.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("yml bean begin .................");
+        System.out.println(ymlBean);
+        System.out.println("yml bean end .................");
+
     }
 }
