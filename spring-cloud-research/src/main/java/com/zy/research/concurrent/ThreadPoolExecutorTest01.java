58247c3fbf81aa52c279d4ebf60f9fc48440bccc
jerry <jerry@zy.com>
2021-01-06 22:15:10 +0800

    线程池监控 & 动态调整参数

--- spring-cloud-research/src/main/java/com/zy/research/concurrent/ThreadPoolExecutorTest01.java
+++ spring-cloud-research/src/main/java/com/zy/research/concurrent/ThreadPoolExecutorTest01.java
@@ -5,7 +5,7 @@ import io.netty.util.concurrent.DefaultThreadFactory;
 
 import java.util.concurrent.*;
 
-public class ThreadPoolExecutorTest {
+public class ThreadPoolExecutorTest01 {
 
     public static void main(String[] args) {
         /*ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.SECONDS,
