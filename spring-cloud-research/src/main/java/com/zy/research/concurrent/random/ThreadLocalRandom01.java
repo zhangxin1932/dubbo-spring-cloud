b5f1b72736a720ac4da37651c982b973b2851ef6
jerry <jerry@zy.com>
2021-01-11 20:33:35 +0800

    各种 ThreadLocal 示例

---
+++ spring-cloud-research/src/main/java/com/zy/research/concurrent/random/ThreadLocalRandom01.java
@@ -0,0 +1,11 @@
+package com.zy.research.concurrent.random;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ThreadLocalRandom01 {
+    public static void main(String[] args) {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        int i = random.nextInt(1, 2);
+        System.out.println(i);
+    }
+}
--- spring-cloud-research/src/main/java/com/zy/research/concurrent/threadlocal/JDKThreadLocal01.java
+++ spring-cloud-research/src/main/java/com/zy/research/concurrent/threadlocal/JDKThreadLocal01.java
@@ -1,5 +1,7 @@
 package com.zy.research.concurrent.threadlocal;
 
+import java.util.concurrent.Semaphore;
+
 public class JDKThreadLocal01 {
 
     private static final ThreadLocal<Integer> THREAD_LOCAL = ThreadLocal.withInitial(() -> 0);
@@ -13,6 +15,10 @@ public class JDKThreadLocal01 {
         t1.join();
         System.out.println(Thread.currentThread().getName() + " --> " + THREAD_LOCAL.get());
         THREAD_LOCAL.remove();
+
+        Semaphore semaphore = new Semaphore(10);
+        semaphore.acquire();
+        semaphore.release();
     }
 
 }
