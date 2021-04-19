026524952060e0dd78a4f7ce3ac2e18c06948236
zhangxin1932 <psy_zhangxin@outlook.com>
2021-04-19 23:22:03 +0800

    消费方调用提供方所有节点并封装执行结果

--- dubbo-spring-cloud-consumer/src/main/java/com/zy/consumer/DubboSpringCloudConsumerApp.java
+++ dubbo-spring-cloud-consumer/src/main/java/com/zy/consumer/DubboSpringCloudConsumerApp.java
@@ -3,10 +3,17 @@ package com.zy.consumer;
 import com.zy.model.DubboReqDTO;
 import com.zy.model.DubboRespDTO;
 import com.zy.service.IDubboService;
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.extension.ExtensionLoader;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.annotation.Method;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.apache.dubbo.rpc.Exporter;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Protocol;
+import org.apache.dubbo.rpc.ProxyFactory;
 import org.apache.dubbo.rpc.service.EchoService;
+import org.apache.dubbo.rpc.service.GenericService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -16,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @SpringBootApplication
@@ -43,6 +51,19 @@ public class DubboSpringCloudConsumerApp {
         // https://www.cnblogs.com/flyingeagle/p/8908317.html
         //
         return iDubboService.dubboCombine(req);
+    }
+
+    @PostConstruct
+    public void init() throws ClassNotFoundException {
+        final String interfaceName = "com.zy.service.IDubboService";
+        ProxyFactory proxyFactory = ExtensionLoader.getExtensionLoader(ProxyFactory.class).getAdaptiveExtension();
+        Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getAdaptiveExtension();
+        URL url = URL.valueOf("dubbo://192.168.0.156.2181/" + interfaceName + "?version=1.0.0");
+        Invoker<?> refer = protocol.refer(Class.forName(interfaceName), url);
+
+        GenericService client = (GenericService) proxyFactory.getProxy(refer, true);
+        Object result = client.$invoke("dubbo", new String[]{"com.zy.model.DubboReqDTO"}, new Object[]{"haha"});
+        System.out.println(result);
     }
 
 }
